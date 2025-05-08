df9927b57af7aea77ae6185c4c41b642a13729e3
kalpesh <kap1210@proton.me>
2025-05-08 10:40:56 +0530

    Using AppCompatActivity instead of FragmentActivity.

--- sample/composeApp/src/androidMain/kotlin/com/mifos/passcode/sample/MainActivity.kt
+++ sample/composeApp/src/androidMain/kotlin/com/mifos/passcode/sample/MainActivity.kt
@@ -2,31 +2,23 @@ package com.mifos.passcode.sample
 
 import App
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.FragmentActivity
 import com.mifos.passcode.LocalCompositionProvider
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
-/*
+
+/**
  * I am using FragmentActivity here because when MainActivity (when AppCompatActivity or ComponentActivity) is attempted to cast into FragmentActivity
  * the app is crashing.
  * Exception: java.lang.ClassCastException: com.mifos.passcode.sample.MainActivity cannot be cast to androidx.fragment.app.FragmentActivity
  **/
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        val androidAuthenticator = AndroidAuthenticator(this)
-//
-//        // Dynamically create a module and register the initialized instance
-//        val activityModule = module {
-//            single { androidAuthenticator }.bind<PlatformAuthenticator>()
-//        }
-//
-//        // Load the module into Koin
-//        loadKoinModules(activityModule)
-
 
         setContent {
             LocalCompositionProvider(this) {
@@ -37,8 +29,8 @@ class MainActivity : FragmentActivity() {
 }
 
 
-//@Preview
-//@Composable
-//fun AppAndroidPreview() {
-//    App()
-//}
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
