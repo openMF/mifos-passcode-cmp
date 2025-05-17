08cf65aa95872fbca185dd877e70e2689d7458ec
kalpesh <pawarkalpesh@proton.me>
2025-05-18 00:40:05 +0530

    Added jsMain as a new source set sample app. It is working.

---
+++ sample/composeApp/src/jsMain/kotlin/com/mifos/passcode/sample/main.kt
@@ -0,0 +1,17 @@
+package com.mifos.passcode.sample
+
+import App
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+import org.jetbrains.skiko.wasm.onWasmReady
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main(){
+    onWasmReady {
+        ComposeViewport(document.body!!) {
+            App()
+        }
+    }
+}
