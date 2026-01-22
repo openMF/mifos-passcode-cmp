d7d522c14916e00a0a0f9ced62fc13ffb720947d
thekalpeshpawar <pawarkalpesh@proton.me>
2026-01-22 12:45:13 +0530

    Update LICENSE path in file headers
    Updated the license file reference in the copyright headers across multiple modules from `LICENSE.md` to `LICENSE`.

--- cmp-sample-shared/src/commonMain/kotlin/cmp/sample/shared/platformAuthentication/AuthenticationScreenViewModel.kt
+++ cmp-sample-shared/src/commonMain/kotlin/cmp/sample/shared/platformAuthentication/AuthenticationScreenViewModel.kt
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * See https://github.com/openMF/mifos-passcode-cmp/blob/development/LICENSE.md
+ * See https://github.com/openMF/mifos-passcode-cmp/blob/development/LICENSE
  */
 package cmp.sample.shared.platformAuthentication
 
