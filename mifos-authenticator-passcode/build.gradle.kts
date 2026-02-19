7755c8f9c35f63c56c193d99909fef879bba9c11
thekalpeshpawar <pawarkalpesh@proton.me>
2026-02-19 15:25:51 +0530

    Update environment variable names for package versioning
    * Updated the environment variable used for setting the version in `mifos-authenticator-biometrics` from `PACKAGE_VERSION` to `BIOMETRICS_PACKAGE_VERSION`.
    * Updated the environment variable used for setting the version in `mifos-authenticator-passcode` from `PACKAGE_VERSION` to `PASSCODE_PACKAGE_VERSION`.

--- mifos-authenticator-passcode/build.gradle.kts
+++ mifos-authenticator-passcode/build.gradle.kts
@@ -42,7 +42,7 @@ kotlin {
 val artifactId = "mifos-authenticator-passcode"
 val mavenGroup: String by project
 val defaultVersion: String by project
-val currentVersion = System.getenv("PACKAGE_VERSION") ?: defaultVersion
+val currentVersion = System.getenv("PASSCODE_PACKAGE_VERSION") ?: defaultVersion
 val desc: String by project
 val license: String by project
 val creationYear: String by project
